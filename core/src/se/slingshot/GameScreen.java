9c28c561e1ec028a184e1616c228df6f6e1579c0
snelleman <emanuelsnelleman@gmail.com>
2015-12-21 20:57:55 +0100

    Add movement

--- core/src/se/slingshot/GameScreen.java
+++ core/src/se/slingshot/GameScreen.java
@@ -6,7 +6,10 @@ import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.math.Vector2;
 import se.slingshot.components.BodyComponent;
+import se.slingshot.components.ControllableComponent;
 import se.slingshot.components.ImageComponent;
+import se.slingshot.systems.ControlSystem;
+import se.slingshot.systems.MovementSystem;
 import se.slingshot.systems.RenderSystem;
 
 /**
@@ -22,6 +25,10 @@ public class GameScreen implements Screen {
     public void show() {
         engine = new PooledEngine();
 
+        ControlSystem controlSystem = new ControlSystem();
+        engine.addSystem(controlSystem);
+        MovementSystem movementSystem = new MovementSystem();
+        engine.addSystem(movementSystem);
         RenderSystem renderSystem = new RenderSystem();
         engine.addSystem(renderSystem);
 
@@ -30,11 +37,12 @@ public class GameScreen implements Screen {
         player.add(new ImageComponent("spaceship_fire.png"));
         player.add(new BodyComponent(
                 new Vector2(3, 3),
-                new Vector2(),
-                new Vector2(),
+                new Vector2(1,0),
+                new Vector2(0,0),
                 2, 2,
                 0
         ));
+        player.add(new ControllableComponent(0,0));
         engine.addEntity(player);
 
         // Debug planet
---
+++ core/src/se/slingshot/systems/MovementSystem.java
@@ -0,0 +1,39 @@
+package se.slingshot.systems;
+
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.Vector2;
+import se.slingshot.components.BodyComponent;
+import se.slingshot.components.ControllableComponent;
+
+/**
+ * Created by emanu on 2015-12-21.
+ */
+public class MovementSystem extends EntitySystem {
+    // ECS
+    private ImmutableArray<Entity> entities;
+    private ComponentMapper<ControllableComponent> controlableMapper = ComponentMapper.getFor(ControllableComponent.class);
+    private ComponentMapper<BodyComponent> bodyMapper = ComponentMapper.getFor(BodyComponent.class);
+
+    @Override
+    public void addedToEngine(Engine engine) {
+        entities = engine.getEntitiesFor(Family.all(ControllableComponent.class, BodyComponent.class).get());
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+        for (int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+            ControllableComponent control = controlableMapper.get(entity);
+            BodyComponent body = bodyMapper.get(entity);
+
+            body.direction.rotate(control.directionThrust * deltaTime);
+            Vector2 tmp = new Vector2(body.direction);
+            body.velocity.add(tmp.scl(control.forwardThrust * deltaTime));
+            tmp = new Vector2(body.velocity);
+            body.position.add(tmp.scl(deltaTime));
+        }
+    }
+
+}
