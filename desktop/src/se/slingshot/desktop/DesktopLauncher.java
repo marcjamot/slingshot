f4c6bc1007b270625f0cac0691c0f3bca4c4a2c4
Marc Jamot <marcjamot@gmail.com>
2015-12-21 15:57:53 +0100

    Add initial files

---
+++ desktop/src/se/slingshot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.slingshot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.slingshot.SlingshotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlingshotGame(), config);
+	}
+}
