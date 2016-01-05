d0e1466319f044734b62b0e51e5abd933bb873b7
Marc Jamot <marcjamot@gmail.com>
2016-01-05 22:17:37 +0100

    Add music, credits to Snabisch @ OpenGameArt

--- core/src/se/slingshot/SlingshotGame.java
+++ core/src/se/slingshot/SlingshotGame.java
@@ -1,16 +1,29 @@
 package se.slingshot;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import se.slingshot.interfaces.ScreenInterface;
 import se.slingshot.menu.ChooseLevelScreen;
 
 public class SlingshotGame extends Game implements ScreenInterface {
     private String loadedLevel;
+    private Music music;
 
     @Override
     public void create() {
         menu();
+        music = Gdx.audio.newMusic(Gdx.files.internal("Lagrima.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        music.dispose();
     }
 
     @Override
